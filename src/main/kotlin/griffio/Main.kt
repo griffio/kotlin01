0b41f90e516e47d361cd11bd9a7f53330a7ed4f6
griffio <griffio@users.noreply.github.com>
2017-08-15 21:01:00 +0100

    Kotlin 1.1.4

--- src/main/kotlin/griffio/Main.kt
+++ src/main/kotlin/griffio/Main.kt
@@ -1,8 +1,14 @@
 package griffio
-/**
- * File compiled as MainKt.class
- */
 
 fun main(args : Array<String>) {
+
     println("Hello, world!")
+
+    val n = 1..10
+    for ( (index, value) in n.withIndex() ) { println("$index $value") }
+
+    for (i in (1..10).filter { it > 5 }) {
+        println(i)
+    }
+
 }
